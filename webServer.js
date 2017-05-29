fd05bbc3d5c5af960efeb2a569832b34aa9eff38
cstock2 <codywstocker@hotmail.com>
2017-05-29 16:45:36 -0700

    more heroku port updates

--- webServer.js
+++ webServer.js
@@ -84,6 +84,9 @@ app.get('/', function(request, response){
     response.send('Simple webserver of files from ' + __dirname);
 });
 
+var port = process.env.port || 3002;
+console.log("PORT: ", port);
+
 var server = app.listen(process.env.port || 3002, function () {
     var port = server.address().port;
     console.log('Listening at http://localhost:' + port + ' exporting the directory ' + __dirname);
